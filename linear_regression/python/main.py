7733f9836ca7950fe673c1badb801e9a7058de24
Vitthal Humbe <vitthal.3454@gmail.com>
2025-12-17 18:22:29 +0530

    restructed the project and added basic class structure.

---
+++ linear_regression/python/main.py
@@ -0,0 +1,36 @@
+
+class LinearRegression:
+    def __init__(self):
+        pass
+    def fit():
+        pass
+    def predict():
+        pass
+    def score():
+        pass
+
+    def _validate_params():
+        pass
+    def _initialize_params():
+        pass
+    def _add_intercept():
+        pass
+
+    def _fit_gd():
+        pass
+    def _fit_sgd():
+        pass
+    def _fit_normal_eq():
+        pass
+
+    def _compute_loss():
+        pass
+    def _compute_gradient():
+        pass
+
+def main():
+    print("Coding started")
+
+
+if __name__ == '__main__':
+    main()
--- src/main.py
+++
@@ -1,5 +0,0 @@
-def main():
-	print("Project setup Complete")
-
-if __name__ == '__main__':
-	main()
