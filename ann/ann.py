e144a9411a7979df47deabb6d9a046422d6a5b57
Vitthal Humbe <vitthal.3454@gmail.com>
2026-01-11 13:23:19 +0530

    Added whole logic of ANN in python; remained : Evaluate()

--- ann/ann.py
+++ ann/ann.py
@@ -1,10 +1,148 @@
 import numpy as np
+from tqdm.auto import tqdm
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 class NeuralNetwrok:
-    pass
+    def __init__(self, *layers):
+        self.layers = list(layers)
+        self.loss = None
+        self.lr = None
+        self.history = {"loss": [], "accuracy": []}
+
+
+    def train(self, X, y,epochs, batch_size=None):
+        n = X.shape[0]
+        
+        for epoch in range(epochs):
+            indices = np.random.permutation(n)
+            X_shuffled = X[indices]
+            y_shuffled = y[indices]
+
+            epoch_loss = 0.0
+
+            if batch_size is None:
+                batches = [(X_shuffled, y_shuffled)]
+            else:
+                # creating batches using list comprehesion :
+                batches = [
+                    (X_shuffled[i:i+batch_size], y_shuffled[i:i+batch_size]) for i in range(0, n, batch_size)
+                ]
+
+            print(f"Epoch {epoch+1}/{epochs}")
+            with tqdm(total=len(batches), ncols=80, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} {postfix}') as pbar:
+                pbar.set_postfix({"loss": "..."}) # Init postfix
+            # training loop : calling forward -> compute loss -> calling backward (update wiehgts)
+                for i, (X_batch, y_batch) in enumerate(batches):
+                    y_hat = self.forward(X_batch)
+
+                    loss = self._compute_loss(y_batch, y_hat)
+                    epoch_loss += loss
+
+                    loss_gradient = self._loss_derivative(y_batch, y_hat)
+                    self.backward(loss_gradient)
+
+                    running_loss = epoch_loss / (i + 1)
+                    pbar.set_postfix({"- loss": f"{running_loss:.4f}"})
+                    pbar.update(1)
+                
+                epoch_loss = epoch_loss / len(batches)
+                self.history["loss"].append(epoch_loss)
+
+                y_pred = self.predict(X)
+                y_pred_labels = np.argmax(y_pred, axis=1)
+                y_true_labels = np.argmax(y, axis=1)
+
+                accuracy = np.mean(y_pred_labels == y_true_labels)
+                self.history["accuracy"].append(accuracy)
+        self.plot_loss_accuracy(
+            self.history["loss"],
+            self.history["accuracy"]
+        )
+        return self.history
+
+                
+    
+    def forward(self, X):
+        out = X
+        for layer in self.layers:
+            out = layer.forward(out)
+
+        return out
+    
+    def backward(self, loss_gradient):
+        gradient = loss_gradient
+        for layer in reversed(self.layers):
+            gradient = layer.backward(gradient)
+
+    def _compute_loss(self, y, y_hat):
+        if self.loss == 'mse':
+            return ((y-y_hat)** 2).mean()
+        else:
+            raise ValueError("Unsupported Loss Function")
+
+    def _loss_derivative(self, y, y_hat):
+        if self.loss == 'mse':
+            return 2 * (y_hat - y) / y.shape[0]
+        else:
+            raise ValueError("Unsupported Loss functin")
+        
+    
+    def compile(self, loss, lr):
+        self.loss = loss
+        self.lr = lr
+        
+        input_dim = None
+
+        for layer in self.layers:
+            if isinstance(layer, Flatten):
+                input_dim = layer.output_dim
+            elif isinstance(layer, Dense):
+                layer._initialize_parameters(input_dim)
+                layer.lr = lr
+                input_dim = layer.n_neurons
+
+    def predict(self, X):
+        return self.forward(X)
+    
+    def plot_loss_accuracy(self,loss, accuracy):
+        epochs = np.arange(1, len(loss) + 1)
+
+        fig, ax1 = plt.subplots(figsize=(8, 5))
+
+        # ---- Loss (left y-axis)
+        ax1.plot(epochs, loss, 'orange', label='Loss')
+        ax1.set_xlabel("Epochs")
+        ax1.set_ylabel("Loss", color='orange')
+        ax1.tick_params(axis='y', labelcolor='r')
+
+        # ---- Accuracy (right y-axis)
+        ax2 = ax1.twinx()
+        ax2.plot(epochs, accuracy, 'b-', label='Accuracy')
+        ax2.set_ylabel("Accuracy", color='b')
+        ax2.tick_params(axis='y', labelcolor='b')
+
+        # ---- Title & grid
+        plt.title("Training Loss & Accuracy")
+
+        plt.show()
+
+    
+
 
 class Flatten:
-    pass
+    def __init__(self, input_shape):
+        self.input_shape = input_shape
+        self.original_shape = None
+        self.output_dim = np.prod(input_shape)
+
+    def forward(self, X):
+        self.original_shape = X.shape
+        return X.reshape(X.shape[0], -1)
+    
+    def backward(self, gradient):
+        return gradient.reshape(self.original_shape)
 
 class Dense:
     def __init__(self, n_nuerons, activation=None):
@@ -68,7 +206,7 @@ class Dense:
             return (Z > 0).astype(float)        # relu'(x) = (0 if x < 0 and 1 if x >= 0)
         
         elif self.activation == 'sigmoid':
-            sigmoid = 1 / (1 - np.exp(-Z))
+            sigmoid = 1 / (1 + np.exp(-Z))
             return sigmoid * (1 - sigmoid)
         
         elif self.activation == 'tanh':
@@ -79,19 +217,114 @@ class Dense:
         
         else:
             raise ValueError("unsupported Activation")
+        
+    def _initialize_parameters(self, input_dim):
+        self.input_dim = input_dim
+
+        if self.activation == 'relu':
+            scale = np.sqrt( 2 / input_dim)         # He
+        else:
+            scale = np.sqrt(1 / input_dim)          # Xavier
+
+        self.weights = np.random.randn(input_dim, self.n_neurons) * scale
+        self.bias = np.zeros(self.n_neurons)
+
+
+
+def main():
+    X, y = fetch_openml("mnist_784",version=1,return_X_y=True,as_frame=False)
+
+    X = X.astype(np.float32)
+    y = y.astype(int)
+    X = X / 255.0
+
+    num_classes = 10
+    y_onehot = np.zeros((y.shape[0], num_classes))
+    y_onehot[np.arange(y.shape[0]), y] = 1
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y_onehot, test_size=0.2, random_state=42
+    )
+
+    model = NeuralNetwrok(
+        Flatten(input_shape=(784,)),
+        Dense(128, activation="relu"),
+        Dense(64, activation="relu"),
+        Dense(10)   # output layer
+    )
+
+    model.compile(loss="mse", lr=0.1)
+
+    history = model.train(
+        X_train,
+        y_train,
+        epochs=11,
+        batch_size=32
+    )
+
+    y_pred = model.predict(X_test)
+    y_pred_labels = np.argmax(y_pred, axis=1)
+    y_true_labels = np.argmax(y_test, axis=1)
+
+    accuracy = np.mean(y_pred_labels == y_true_labels)
+
+    print("\nFinal Test Accuracy:", accuracy)
+
 if __name__ == '__main__':
     np.random.seed(42)
 
-    X = np.random.randn(5, 3)          # 5 rows and 3 columns dataset
-    deriv_A = np.random.randn(5, 3)
+    main()
 
-    dense = Dense(3, activation='relu')
-    dense.weights = np.random.randn(3, 3)
-    dense.bias = np.zeros(3)
-    dense.lr = 0.01
+    # X = np.random.randn(5, 3)          # 5 rows and 3 columns dataset
+    # deriv_A = np.random.randn(5, 3)
 
-    out = dense.forward(X)
-    dX = dense.backward(deriv_A)
-    print(dX.shape)        # should be 5 rows and 4 cols
-    print(dX)
+    # dense = Dense(3, activation='relu')
+    # dense.weights = np.random.randn(3, 3)
+    # dense.bias = np.zeros(3)
+    # dense.lr = 0.01
+
+    # out = dense.forward(X)
+    # dX = dense.backward(deriv_A)
+    # print(dX.shape)        # should be 5 rows and 4 cols
+    # print(dX)
+
+    # model = NeuralNetwrok(
+    #     Flatten(input_shape=(3,)),
+    #     Dense(5, activation='relu'),
+    #     Dense(1)
+    # )
+
+    # model.compile(loss='mse', lr=0.01)
+
+    # X = np.random.randn(10, 3)
+    # y_hat = model.predict(X)
+
+    # print(y_hat)
+    # print(y_hat.shape)
+
+    # X = np.random.randn(100000, 1)
+    # y = (X ** 2).reshape(-1, 1)
+
+    # X = (X - X.mean()) / X.std()
+    # y = (y - y.mean()) / y.std()
+
+    # y_mean = y.mean()
+    # y_std = y.std()
+
+    # model = NeuralNetwrok(
+    #     Flatten(input_shape=(1,)),
+    #     Dense(16, activation='relu'),
+    #     Dense(16, activation='tanh'),
+    #     Dense(1)
+    # )
+
+    # model.compile(loss='mse', lr= 0.001)
+    # history = model.train(X, y, epochs=11, batch_size=32)
+
+    # print(history['loss'][-1])
+    # y_pred = model.predict(X[:5])
+    # y_pred_real = y_pred * y_std + y_mean
+
+    # print(y_pred_real)
+    # print(y[:5])
 
