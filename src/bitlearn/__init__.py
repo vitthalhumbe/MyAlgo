1b289484ad5ac6d0cfe9d60a35237da5db8e167f
vitthalhumbe <vitthal.3454@gmail.com>
2026-08-07 10:30:39 +0530

    added some things

--- src/bitlearn/__init__.py
+++ src/bitlearn/__init__.py
@@ -5,5 +5,5 @@ from bitlearn.linear_regression import LinearRegression
 from bitlearn.ann import NeuralNetwork, Dense, Flatten
 from bitlearn.neuron import Neuron
 
-__version__ = "0.1.0"
+__version__ = "1.0.4"
 __all__ = ["LinearRegression", "NeuralNetwork", "Dense", "Flatten", "Neuron"]
